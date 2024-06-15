ff75eb45b98b546442affa8d00619a0b1a9a7fac
Jeevanksbu <jeevan.kunapareddy@stonybrook.edu>
2024-06-15 09:47:57 -0400

    modifications 3 for assignment-5-part-1

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,38 +1,18 @@
-#! /bin/sh
+#!/bin/sh
 
 case "$1" in
     start)
-        echo "Starting aesdsocket"
-        ./aesdsocket > aesdsocket_log.txt 2>&1 &
-        AESDSOCKET_PID=$!
-        echo "aesdsocket PID: $AESDSOCKET_PID"
+        echo "Starting"
+        start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
         ;;
     stop)
-        echo "Stopping aesdsocket"
-
-        # First, find and kill the aesdsocket process
-        PIDS=$(pgrep -f "./aesdsocket")
-        if [ -n "$PIDS" ]; then
-            for PID in $PIDS; do
-                kill -9 "$PID"
-                echo "aesdsocket process (PID: $PID) stopped."
-            done
-        else
-            echo "aesdsocket is not running."
-        fi
-
-        # Now, kill the script itself
-        SCRIPT_PID=$(pgrep -f "./aesdsocket-start-stop.sh")
-        if [ -n "$SCRIPT_PID" ]; then
-            kill -9 "$SCRIPT_PID"
-            echo "Script (PID: $SCRIPT_PID) stopped."
-        else
-            echo "Script is not running."
-        fi
+        echo "Stopping"
+        start-stop-daemon -K -n aesdsocket
         ;;
     *)
         echo "Usage: $0 {start|stop}"
-        exit 1
+    	exit 1
+	;;
 esac
 
 exit 0
